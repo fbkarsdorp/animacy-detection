aaa1765a5e14c22c00845b1f0e57cffcea563e81
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-05 18:08:46 +0100

    more elaborate output

--- learning_curve.py
+++ learning_curve.py
@@ -1,5 +1,6 @@
 import sys
-
+import matplotlib
+matplotlib.use('Agg')
 import seaborn as sb
 import numpy as np
 import pandas as pd
@@ -25,25 +26,41 @@ y_train_docs = [label for i in y_train_idx for label in y[i]]
 X_test_docs = [X[i] for i in X_test_idx]
 y_test_docs = [label for i in y_test_idx for label in y[i]]
 
-scores = np.zeros((10, 3))
-sizes = []
-for i, train_size in enumerate(np.arange(0.1, 1.1, 0.1)):
-    size = int(len(X_train_docs) * train_size)
-    sizes.append(size)
-    X_train = include_features(X_train_docs[:size], ('word', 'pos'))
-    X_test = include_features(X_test_docs, ('word', 'pos'))
-    features = FeatureStacker(('windower', Windower(window_size=3)),
-                              ('embeddings', WordEmbeddings(model)))
-    X_train = features.fit_transform(X_train)
-    X_test = features.transform(X_test)
-    le = LabelEncoder()
-    y_train = le.fit_transform(y_train_docs[:X_train.shape[0]])
-    y_test = le.transform(y_test_docs)
-    clf = LogisticRegression(C=1.0)
-    clf.fit(X_train, y_train)
-    preds = clf.predict(X_test)
-    scores[i] = precision_recall_fscore_support(y_test, preds, average='micro')[:3]
 
-df = pd.DataFrame(scores, index=sizes, columns=['precision', 'recall', 'F-score'])
-df.plot()
+experiments = [('word',), ('word', 'pos'), ('word', 'pos', 'root'),
+               ('word', 'pos', 'root', 'rel')]
+experiments = experiments + [experiment + ('embeddings', )
+                             for experiment in experiments]
+experiments += [('embeddings', )]
+
+scores = np.zeros((10, len(experiments)))
+sizes = []
+
+for e, experiment in enumerate(experiments):
+    print experiment
+    for i, train_size in enumerate(np.arange(0.1, 1.1, 0.1)):
+        size = int(len(X_train_docs) * train_size)
+        sizes.append(size)
+        if experiment == ('embeddings', ):
+            features = FeatureStacker(('embeddings', WordEmbeddings(model)))
+            experiment = ('word', ) + experiment
+        else:
+            features = FeatureStacker(('windower', Windower(window_size=3)),
+                                      ('embeddings', WordEmbeddings(model)))
+        X_train = include_features(X_train_docs[:size], experiment)
+        X_test = include_features(X_test_docs, experiment)
+        X_train = features.fit_transform(X_train)
+        X_test = features.transform(X_test)
+        le = LabelEncoder()
+        y_train = le.fit_transform(y_train_docs[:X_train.shape[0]])
+        y_test = le.transform(y_test_docs)
+        clf = LogisticRegression(C=1.0)
+        clf.fit(X_train, y_train)
+        preds = clf.predict(X_test)
+    scores[i, e] = precision_recall_fscore_support(y_test, preds, average='micro')[2]
+
+df = pd.DataFrame(scores, index=sizes,
+                  columns=['_'.join(exp) for exp in experiments])
+fig = sb.plt.figure()
+ax = df.plot()
 sb.plt.savefig("learning_curve.pdf")
